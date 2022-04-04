f29540422f59dcb99bcaf4d954cc2f05a7494bdd
Octavian Gavril <100153009+octaviangavril@users.noreply.github.com>
2022-04-04 18:48:58 +0300

    Add files via upload

---
+++ Laborator7/Problema1/Source.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+float operator""_Kelvin(unsigned long long int x)
+{
+    return x - 273.15;
+}
+
+float operator""_Fahrenheit(unsigned long long int x)
+{
+    return (x - 32) / 1.8;
+}
+
+int main() {
+
+    float a = 300_Kelvin;
+
+    float b = 120_Fahrenheit;
+
+    printf("%f %f", a, b);
+    return 0;
+
+}
